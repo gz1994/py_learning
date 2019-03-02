2b01dbc65c062d6efbc3d1d27cdd6f58a356edb8
gz1994 <904666805@qq.com>
2019-03-02 16:35:01 +0800

    SevenDigitalDraw和hanoi是关于代码复用和递归的程序

---
+++ Code Reuse and Recursion/SevenDigitalDraw.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import turtle,time,pytz     #绘制七段数码管
+def drawGap():  #绘制数码管间隔
+    turtle.penup()
+    turtle.fd(5)
+def drawLine(draw): #绘制单个数码管
+    drawGap()
+    turtle.pendown() if draw else turtle.penup()
+    turtle.fd(40)
+    drawGap()
+    turtle.right(90)
+
+def drawDigit(digit):   #根据数字绘制7段数码管
+    drawLine(True) if digit in [2, 3, 4, 5, 6, 8, 9] else drawLine(False)
+    drawLine(True) if digit in [0, 1, 3, 4, 5, 6, 7, 8, 9] else drawLine(False)
+    drawLine(True) if digit in [0, 2, 3, 5, 6, 8, 9] else drawLine(False)
+    drawLine(True) if digit in [0, 2, 6, 8] else drawLine(False)
+    turtle.left(90)
+    drawLine(True) if digit in [0, 4, 5, 6, 8, 9] else drawLine(False)
+    drawLine(True) if digit in [0, 2, 3, 5, 6, 7, 8, 9] else drawLine(False)
+    drawLine(True) if digit in [0, 1, 2, 3, 4, 7, 8, 9] else drawLine(False)
+    turtle.left(180)
+    turtle.penup()
+    turtle.fd(20)
+def drawDate(date):
+    turtle.pencolor("red")
+    for i in date:
+        if i == '-':
+            turtle.write('年',font=("Arial",18,"normal"))
+            turtle.pencolor("green")
+            turtle.fd(40)
+        elif i == '=':
+            turtle.write('月', font=("Arial", 18, "normal"))
+            turtle.pencolor("blue")
+            turtle.fd(40)
+        elif i == '+':
+            turtle.write('日', font=("Arial", 18, "normal"))
+            turtle.fd(40)
+        elif i == ':':
+            turtle.write(':', font=("Arial", 18, "normal"))
+            turtle.fd(40)
+        else:
+            drawDigit(eval(i))
+def main():
+
+    turtle.setup(1600, 350, 200, 200)
+    turtle.penup()
+    turtle.speed(10)
+    turtle.fd(-500)
+    turtle.pensize(5)
+    #drawDate("1994-01=17+")
+    drawDate(time.strftime('%Y-%m=%d+%H:%M:%S',time.localtime()))  #打印系统时间
+    turtle.hideturtle()
+    turtle.done()
+
+main()
---
+++ Code Reuse and Recursion/hanoi.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+count = 0
+def hanoi(n, src, dst, mid): #n代表圆盘个数，src代表第一个塔，
+                            # dst代表目标塔，mid代表中间塔
+    global count
+    if n == 1 :
+        print("{}:{}->{}".format(1,src,dst))
+        count += 1
+    else :
+        hanoi(n-1, src, mid, dst)
+        print("{}:{}->{}".format(n, src, dst))
+        count += 1
+        hanoi(n-1, mid, dst, src)
+hanoi(3, "A", "C","B")
+print(count)
+

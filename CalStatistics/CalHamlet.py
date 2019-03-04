6f2804552af8af01e094a0d82aeb5cf004f5d81a
gz1994 <904666805@qq.com>
2019-03-04 11:12:36 +0800

    增加注释说明

--- CalStatistics/CalHamlet.py
+++ CalStatistics/CalHamlet.py
@@ -3,18 +3,18 @@
 #CalHamlet.py
 def getText():
     txt = open("hamlet.txt","r").read()
-    txt = txt.lower()
+    txt = txt.lower()       #把文章内的大写字母变成小写
     for ch in '!"#$%&*()+,./:;<=>?@[\\]^_`~·{|}':
-        txt = txt.replace(ch," ")
+        txt = txt.replace(ch," ")       #清除特殊字符并用空格代替
     return txt
 
 hamletTxt = getText()
-words = hamletTxt.split()
-counts = {}
+words = hamletTxt.split()           #切片处理
+counts = {}                         #建立一个空字典
 for word in words:
     counts[word] = counts.get(word,0) + 1
-items = list(counts.items())
-items.sort(key=lambda x:x[1],reverse=True)
+items = list(counts.items())        #字典类型转换成列表类型
+items.sort(key=lambda x:x[1],reverse=True)#按照值进行排序，默认从小到大
 for i in range(10):
     word, count = items[i]
     print("{0:<10}{1:>5}".format(word, count))

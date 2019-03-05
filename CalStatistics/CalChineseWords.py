076e3c219370ec24485b52dc0252bf96771cd4c3
gz1994 <904666805@qq.com>
2019-03-05 09:11:49 +0800

    第一次上传中文切片，统计词频程序

---
+++ CalStatistics/CalChineseWords.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#CalChineseWords.py 中文文本分词，使用字典表达词频
+import jieba
+txt = open("threekingdoms.txt","r",encoding="utf-8").read()
+words = jieba.lcut(txt)
+counts = {}
+for word in words:
+    if len(word) == 1:
+        continue
+    else:
+        counts[word] = counts.get(word, 0) + 1
+items = list(counts.items())
+items.sort(key=lambda x:x[1],reverse=True)
+for i in range(15):
+    word, count = items[i]
+    print("{0:<10}{1:>5}".format(word,count))
+
+
+
